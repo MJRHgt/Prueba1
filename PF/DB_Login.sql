546571f6eda2ad7e1931e28d5e9742055c6ac4d5
Mario Jose Roldan <mariojudoka@gmail.com>
2020-03-18 15:50:50 -0600

    commint
    es el control llevo P1, donde intente usar codigo con php
    En P2, donde utilize c# y sql Server, cosa que nunca habia utilizado, pero me genera error en la conexion

---
+++ PF/DB_Login.sql
@@ -0,0 +1,9 @@
+CREATE TABLE 'usuarios' (
+  'idusuario' INT(11) NOT NULL AUTO_INCREMENT,
+  'usser' VARCHAR(20) NOT NULL,
+  'password' VARCHAR(10) NOT NULL,
+  'password' VARCHAR(10) NOT NULL,
+  'rol' VARCHAR(20),
+  'correo' VARCHAR(50)
+  PRIMARY KEY  ('idusuario')
+)
